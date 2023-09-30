b8798a67a975e8c9332dce62b06832cfe74c968a
Nathan Stouffer <nathanstouffer1999@gmail.com>
2023-09-30 14:21:30 -0600

    start setting up a vector class

---
+++ src/stf/math/vector.h
@@ -0,0 +1,79 @@
+#pragma once
+
+namespace stf {
+namespace math {
+    
+    template<typename T, size_t N>
+    struct vec
+    {
+    public:
+
+        T[n] values;
+
+        vec() : vec(T(0)) {}
+        vec(T value)
+        {
+            for (size_t i = 0; i < N; ++i)
+            {
+                values[i] = value;
+            }
+        }
+
+        inline T const& operator[](size_t i) const { return values[i]; }
+        inline T& operator[](size_t i) { return values[i]; }
+
+        inline size_t size() const { return N; }
+
+        template<typename U>
+        vec<U, N> as() const
+        {
+            vec<U, N> result;
+            for (size_t i = 0; i < N; ++i)
+            {
+                result[i] = static_cast<U>(values[i]);
+            }
+            return result;
+        }
+
+    public:
+
+        inline vec<T, N>& operator+=(vec<T, N> const& rhs)
+        {
+            for (size_t i = 0; i < N; ++i)
+            {
+                values[i] += rhs[i]; 
+            }
+            return *this;
+        }
+
+        inline vec<T, N>& operator-=(vec<T, N> const& rhs)
+        {
+            for (size_t i = 0; i < N; ++i)
+            {
+                values[i] -= rhs[i]; 
+            }
+            return *this;
+        }
+
+    public:
+
+        static inline size_t byte_size() { return sizeof(T) * N; }
+
+    };
+
+    template<typename T, size_t N>
+    inline vec<T, N> operator+(vec<T, N> const& lhs, vec<T, N> const& rhs)
+    {
+        vec<T, N> result;
+        return result += lhs += rhs;
+    }
+
+    template<typename T, size_t N>
+    inline vec<T, N> operator-(vec<T, N> const& lhs, vec<T, N> const& rhs)
+    {
+        vec<T, N> result;
+        return result -= lhs -= rhs;
+    }
+
+} // math
+} // stf
---
+++ src/stf/types.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "math/vector.h"
+
+namespace stf {
+
+    using gpu_float_t = float; 
+
+    template<typename T>
+    struct instantiate_types
+    {
+
+        // vector types
+        typedef math::vec<T, 2> vec2;
+        typedef math::vec<T, 3> vec3;
+        typedef math::vec<T, 4> vec4;
+
+    };
+
+    using gpu = instantiate_types<gpu_float_t>
+
+} // stf
