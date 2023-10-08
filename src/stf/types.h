7796794fa8fb6c1bc6104985f84329c801bbdce8
Nathan Stouffer <nathanstouffer1999@gmail.com>
2023-10-08 10:20:58 -0600

    reduce verbosity

--- src/stf/types.h
+++ src/stf/types.h
@@ -4,26 +4,30 @@
 #include "math/matrix.h"
 #include "math/vector.h"
 
-namespace stf {
+using gpu_float_t = float;
 
-    using gpu_float_t = float;
+namespace stf {
 
     template<typename T>
     struct instantiate_types
     {
 
         // vector types
-        typedef math::vec<T, 2> vec2;
-        typedef math::vec<T, 3> vec3;
-        typedef math::vec<T, 4> vec4;
+        using vec2 = math::vec<T, 2>;
+        using vec3 = math::vec<T, 3>;
+        using vec4 = math::vec<T, 4>;
 
         // matrix types
-        typedef math::mtx<T, 2> mtx2;
-        typedef math::mtx<T, 3> mtx3;
-        typedef math::mtx<T, 4> mtx4;
+        using mtx2 = math::mtx<T, 2>;
+        using mtx3 = math::mtx<T, 3>;
+        using mtx4 = math::mtx<T, 4>;
 
     };
 
     using gpu = instantiate_types<gpu_float_t>;
 
 } // stf
+
+// default instantiated types for projects that don't have to worry about precision
+using stff = stf::instantiate_types<float>;
+using stfd = stf::instantiate_types<double>;
--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -32,7 +32,7 @@ std::ostream& operator<<(std::ostream& s, stf::math::mtx<T, N> const& rhs)
 int main()
 {
     {
-        stf::gpu::vec2 tmp;
+        stff::vec2 tmp;
         std::cout << "tmp: " << tmp << std::endl;
 
         tmp.x = 1;
@@ -41,7 +41,7 @@ int main()
         tmp += tmp;
         std::cout << "+=: " << tmp << std::endl;
 
-        stf::gpu::vec2 other;
+        stff::vec2 other;
         other.y = 2;
         std::cout << "other: " << other << std::endl;
 
@@ -50,7 +50,7 @@ int main()
     }
 
     {
-        stf::gpu::vec3 tmp(8);
+        stff::vec3 tmp(8);
         tmp.z = 9;
 
         std::cout << "cast: " << tmp.as<double>() << std::endl;
@@ -58,27 +58,27 @@ int main()
     }
 
     {
-        stf::gpu::vec2 lhs(1);
-        stf::gpu::vec2 rhs(2);
+        stff::vec2 lhs(1);
+        stff::vec2 rhs(2);
         std::cout << lhs << " * " << rhs << " = " << lhs * rhs << std::endl;
         std::cout << std::endl;
     }
 
     {
-        stf::gpu::vec2 tmp(5);
+        stff::vec2 tmp(5);
         std::cout << "length(" << tmp << ") = " << tmp.length() << std::endl;
         std::cout << "normalize(" << tmp << ") = " << tmp.normalized() << std::endl;
         std::cout << std::endl;
     }
 
     {
-        stf::gpu::vec2 tmp(5);
+        stff::vec2 tmp(5);
         std::cout << "2 * " << tmp << " = " << 2.f * tmp << std::endl;
         std::cout << std::endl;
     }
 
     {
-        stf::gpu::mtx2 tmp(1);
+        stff::mtx2 tmp(1);
         std::cout << "tmp: " << std::endl << tmp << std::endl;
         std::cout << "tmp[0][0] = " << tmp[0][0] << std::endl;
         tmp[0][0] = 3;
@@ -87,17 +87,17 @@ int main()
     }
 
     {
-        stf::gpu::mtx2 tmp;
+        stff::mtx2 tmp;
         std::cout << "tmp: " << std::endl << tmp << std::endl;
-        tmp[0] = stf::gpu::vec2(3, 4);
+        tmp[0] = stff::vec2(3, 4);
         std::cout << std::endl << "write [3, 4] to first row" << std::endl << tmp << std::endl;
         std::cout << "transpose: " << std::endl << tmp.transposed() << std::endl;
         std::cout << std::endl;
     }
 
     {
-        stf::gpu::mtx2 lhs(1);
-        stf::gpu::mtx2 rhs(2);
+        stff::mtx2 lhs(1);
+        stff::mtx2 rhs(2);
         std::cout << "lhs: " << std::endl << lhs << std::endl;
         std::cout << "rhs: " << std::endl << rhs << std::endl;
         std::cout << std::endl << "multiply" << std::endl << lhs * rhs << std::endl;
@@ -105,12 +105,12 @@ int main()
     }
 
     {
-        stf::gpu::mtx4 lhs;
+        stff::mtx4 lhs;
         lhs[0][0] = 0; lhs[0][1] = 4; lhs[0][2] = 8;  lhs[0][3] = 12;
         lhs[1][0] = 1; lhs[1][1] = 5; lhs[1][2] = 9;  lhs[1][3] = 13;
         lhs[2][0] = 2; lhs[2][1] = 6; lhs[2][2] = 10; lhs[2][3] = 14;
         lhs[3][0] = 3; lhs[3][1] = 7; lhs[3][2] = 11; lhs[3][3] = 15;
-        stf::gpu::mtx4 rhs;
+        stff::mtx4 rhs;
         rhs[0][0] = 0; rhs[0][1] = 4; rhs[0][2] = 8;  rhs[0][3] = 12;
         rhs[1][0] = 1; rhs[1][1] = 5; rhs[1][2] = 9;  rhs[1][3] = 13;
         rhs[2][0] = 2; rhs[2][1] = 6; rhs[2][2] = 10; rhs[2][3] = 14;
@@ -122,22 +122,22 @@ int main()
     }
 
     {
-        stf::gpu::vec3 tmp(1, 0, 0);
+        stff::vec3 tmp(1, 0, 0);
         std::cout << "tmp: " << tmp << std::endl;
-        std::cout << "rotated: " << stf::math::rotate(tmp, stf::gpu::vec3(0, 0, 1), stf::constants<stf::gpu_float_t>::half_pi) << std::endl;
-        std::cout << "rotated: " << stf::math::rotate(stf::gpu::vec3(0, 0, 1), stf::constants<stf::gpu_float_t>::half_pi) * stf::gpu::vec4(tmp, 0) << std::endl;
+        std::cout << "rotated: " << stf::math::rotate(tmp, stff::vec3(0, 0, 1), stf::constants<float>::half_pi) << std::endl;
+        std::cout << "rotated: " << stf::math::rotate(stff::vec3(0, 0, 1), stf::constants<float>::half_pi) * stff::vec4(tmp, 0) << std::endl;
         std::cout << std::endl;
     }
 
     {
-        stf::gpu::vec3 tmp(0, -1, 0);
-        stf::gpu_float_t constexpr delta_phi = -stf::constants<stf::gpu_float_t>::quarter_pi ;
-        stf::gpu_float_t constexpr delta_theta= stf::constants<stf::gpu_float_t>::half_pi;
+        stff::vec3 tmp(0, -1, 0);
+        float constexpr delta_phi = -stf::constants<float>::quarter_pi ;
+        float constexpr delta_theta= stf::constants<float>::half_pi;
         std::cout << "tmp: " << tmp << "   delta_phi: -pi/4    delta_theta: pi/2" << std::endl;
-        std::cout << "desired: " << stf::gpu::vec3(0.707107, 0, 0.707107) << std::endl;
-        stf::gpu::mtx4 mtx = stf::math::orbit(stf::gpu::vec3(), stf::gpu::vec3(1, 0, 0), delta_phi, delta_theta);
-        std::cout << "orbited:" << mtx * stf::gpu::vec4(tmp, 1) << std::endl;
-        std::cout << "orbited:" << stf::math::orbit(tmp, stf::gpu::vec3(), stf::gpu::vec3(1, 0, 0), delta_phi, delta_theta) << std::endl;
+        std::cout << "desired: " << stff::vec3(0.707107, 0, 0.707107) << std::endl;
+        stff::mtx4 mtx = stf::math::orbit(stff::vec3(), stff::vec3(1, 0, 0), delta_phi, delta_theta);
+        std::cout << "orbited:" << mtx * stff::vec4(tmp, 1) << std::endl;
+        std::cout << "orbited:" << stf::math::orbit(tmp, stff::vec3(), stff::vec3(1, 0, 0), delta_phi, delta_theta) << std::endl;
         std::cout << std::endl;
     }
 }
