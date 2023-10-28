4f5371fb8981c0950d5c9627841013baf27d52de
Nathan Stouffer <nathanstouffer1999@gmail.com>
2023-10-28 13:33:04 -0600

    add (untested orbit functionality to the camera

--- code/src/stf/include/interface/stf/alg/transform.hpp
+++ code/src/stf/include/interface/stf/alg/transform.hpp
@@ -30,6 +30,8 @@ namespace alg {
     }
 
     // NOTE: we assume angles are from the perspective of the focus point and that right is a unit vector
+    // we take the origin to be the focus point and the coordinate axes defined by the right/look/up vectors of the input camera stat.
+    // orbiting is achieved by first rotating about the right direction and then around (0, 0, 1)
     template<typename T>
     inline math::vec3<T> orbit(math::vec3<T> const& val, math::vec3<T> const& focus, math::vec3<T> const& right, T const delta_phi, T const delta_theta)
     {
@@ -40,7 +42,19 @@ namespace alg {
     }
 
     template<typename T>
-    inline cam::scamera<T> orbit(cam::scamera<T> const& camera, math::vec3<T>);
+    inline cam::scamera<T> orbit(cam::scamera<T> const& camera, math::vec3<T> const& focus, T const delta_phi, T const delta_theta)
+    {
+        // copy camera state so unaffected values are preserved
+        cam::scamera<T> result = camera;
+
+        // rotate the eye and adjust heading and pitch appropriately
+        result.eye = orbit(result.eye, focus, camera.right(), -delta_phi, delta_theta);
+        result.theta += delta_theta;
+        result.phi += delta_phi;
+
+        // return result
+        return result;
+    }
 
 } // alg
 } // stf
