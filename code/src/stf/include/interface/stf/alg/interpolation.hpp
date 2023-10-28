16192cc3f93da10c413afae6a861f18fa4b2be63
Nathan Stouffer <nathanstouffer1999@gmail.com>
2023-10-28 13:45:52 -0600

    add interpolation for camera

--- code/src/stf/include/interface/stf/alg/interpolation.hpp
+++ code/src/stf/include/interface/stf/alg/interpolation.hpp
@@ -4,11 +4,28 @@
 
 #include <algorithm>
 
+#include "stf/alg/spherical.hpp"
+#include "stf/cam/scamera.hpp"
 #include "stf/gfx/color.hpp"
 #include "stf/math/vector.hpp"
 
 namespace stf {
 namespace alg {
+
+    // clamp time to [0, 1]
+    template<typename T>
+    inline T clamp_time(T t)
+    {
+        return std::clamp(t, T(0), T(1));    // clamped
+    }
+
+    // smooth out time for smoothstep
+    template<typename T>
+    inline T smooth_time(T t)
+    {
+        T t = clamp_time(t);                // clamped
+        return t * t * (T(3) - T(2) * t);   // smoothed
+    }
 
     // multiply and add
     template<typename T>
@@ -37,17 +54,15 @@ namespace alg {
     }
 
     template<typename T>
-    inline T lerpstep(T a, T b, T x)
+    inline T lerpstep(T a, T b, T t)
     {
-        return lerp(a, b, std::clamp(x, T(0), T(1)));
+        return lerp(a, b, clamp_time(t));
     }
 
     template<typename T>
-    inline T smoothstep(T a, T b, T x)
+    inline T smoothstep(T a, T b, T t)
     {
-        T t = std::clamp(t, T(0), T(1));    // clamped
-        T s = t * t * (T(3) - T(2) * t);    // smooth
-        return lerp(a, b, s);
+        return lerp(a, b, smooth_time(t));
     }
 
     template<typename T>
@@ -87,52 +102,63 @@ namespace alg {
     template<class T, size_t N>
     inline math::vec<T, N> lerpstep(math::vec<T, N> const& a, math::vec<T, N> const& b, T const t)
     {
-        math::vec<T, N> result;
-        for (size_t i = 0; i < N; ++i)
-        {
-            result[i] = lerpstep(a[i], b[i], t);
-        }
-        return result;
+        return lerp(a, b, clamp_time(t))
     }
 
     template<class T, size_t N>
     inline math::vec<T, N> smoothstep(math::vec<T, N> const& a, math::vec<T, N> const& b, T const t)
     {
-        math::vec<T, N> result;
-        for (size_t i = 0; i < N; ++i)
-        {
-            result[i] = smoothstep(a[i], b[i], t);
-        }
-        return result;
+        return lerp(a, b, smooth_time(t));
     }
 
     inline gfx::rgba clamp(gfx::rgba const& lhs, gfx::rgba::num_t min, gfx::rgba::num_t max)
     {
-        using vec_t = math::vec<gfx::rgba::num_t, 4>;
-        return gfx::rgba(clamp(static_cast<vec_t>(lhs), min, max));
+        return gfx::rgba(clamp(lhs.as_vec(), min, max));
     }
 
     inline gfx::rgba lerp(gfx::rgba const& lhs, gfx::rgba const& rhs, gfx::rgba::num_t t)
     {
-        using vec_t = math::vec<gfx::rgba::num_t, 4>;
-        return gfx::rgba(lerp(static_cast<vec_t>(lhs), static_cast<vec_t>(rhs), t));
+        return gfx::rgba(lerp(lhs.as_vec(), rhs.as_vec(), t));
     }
 
     inline gfx::rgba lerpstep(gfx::rgba const& lhs, gfx::rgba const& rhs, gfx::rgba::num_t t)
     {
-        using vec_t = math::vec<gfx::rgba::num_t, 4>;
-        return gfx::rgba(lerpstep(static_cast<vec_t>(lhs), static_cast<vec_t>(rhs), t));
+        return gfx::rgba(lerpstep(lhs.as_vec(), rhs.as_vec(), t));
     }
 
     inline gfx::rgba smoothstep(gfx::rgba const& lhs, gfx::rgba const& rhs, gfx::rgba::num_t t)
     {
-        using vec_t = math::vec<gfx::rgba::num_t, 4>;
-        return gfx::rgba(smoothstep(static_cast<vec_t>(lhs), static_cast<vec_t>(rhs), t));
+        return gfx::rgba(smoothstep(lhs.as_vec(), rhs.as_vec(), t));
     }
 
     // TODO (stouff) write this blend function 
     //inline gfx::rgba blend(gfx::rgba const& current, gfx::rgba const& writing)
 
+    template<typename T>
+    inline cam::scamera<T> lerp(cam::scamera<T> const& lhs, cam::scamera<T> const& rhs, T const t)
+    {
+        cam::scamera<T> result;
+        result.eye    = lerp(lhs.eye, rhs.eye, t);
+        result.theta  = lerp(lhs.theta, closest_equiv_angle(lhs.theta, rhs.theta), t);
+        result.phi    = lerp(lhs.phi, closest_equiv_angle(lhs.phi, rhs.phi), t);
+        result.near   = lerp(lhs.near, rhs.far, t);
+        result.far    = lerp(lhs.far, rhs.far, t);
+        result.aspect = lerp(lhs.aspect, rhs.aspect, t);
+        result.fov    = lerp(lhs.fov, rhs.fov, t);
+        return result;
+    }
+
+    template<typename T>
+    inline cam::scamera<T> lerpstep(cam::scamera<T> const& lhs, cam::scamera<T> const& rhs, T const t)
+    {
+        return lerp(lhs, rhs, clamp_time(t));
+    }
+
+    template<typename T>
+    inline cam::scamera<T> smoothstep(cam::scamera<T> const& lhs, cam::scamera<T> const& rhs, T const t)
+    {
+        return lerp(lhs, rhs, smooth_time(t));
+    }
 
 } // alg
 } // stf
