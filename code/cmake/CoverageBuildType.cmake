e5c199bc2eba794c29f2ade903dfb1375a3b4b9b
Nathan Stouffer <nathanstouffer1999@gmail.com>
2023-11-02 19:58:13 -0600

    provide coverage flags at link time

--- code/cmake/CoverageBuildType.cmake
+++ code/cmake/CoverageBuildType.cmake
@@ -15,6 +15,6 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 
     set(CMAKE_C_FLAGS_COVERAGE "${CMAKE_C_FLAGS_DEBUG} -fprofile-instr-generate -fcoverage-mapping -fno-inline -fno-elide-constructors")
     set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_DEBUG} -fprofile-instr-generate -fcoverage-mapping -fno-inline -fno-elide-constructors")
-    set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
-    set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}")
+    set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -fprofile-instr-generate -fcoverage-mapping")
+    set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -fprofile-instr-generate -fcoverage-mapping")
 endif()
