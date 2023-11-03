ab1194ad4c1a7c508766eef6ee19c4ae2a085eac
Nathan Stouffer <nathanstouffer1999@gmail.com>
2023-11-02 20:54:50 -0600

    remove temporary directory

--- tmp/coverage.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-clang++ -fprofile-instr-generate -fcoverage-mapping simple.cpp -o simple
-./simple
-llvm-profdata merge -sparse default.profraw -o simple.profdata
-llvm-cov show ./simple -instr-profile=simple.profdata
