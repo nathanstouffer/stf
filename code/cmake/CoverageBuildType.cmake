bd37617a8436af8e86ae8393360a649b36a11fba
Nathan Stouffer <nathanstouffer1999@gmail.com>
2023-11-02 18:55:41 -0600

    change coverage to test for clang compiler

--- code/cmake/CoverageBuildType.cmake
+++ code/cmake/CoverageBuildType.cmake
@@ -1,5 +1,5 @@
-# set up the coverage config -- this is gcc only
-if (CMAKE_COMPILER_IS_GNUCXX)
+# set up the coverage config -- this is clang only
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     get_property(IS_MULTI_CONFIG GLOBAL PROPERTY GENERATOR_IS_MULTI)
     if (IS_MULTI_CONFIG)
         if (NOT "Coverage" IN_LIST CMAKE_CONFIGURATION_TYPES)
@@ -17,4 +17,5 @@ if (CMAKE_COMPILER_IS_GNUCXX)
     set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_DEBUG} -fprofile-instr-generate -fcoverage-mapping -fno-inline -fno-inline-small-functions -fno-default-inline -fno-elide-constructors")
     set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
     set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}")
+    message(STATUS "PRINT DEBUGGING WAS HERE")
 endif()
