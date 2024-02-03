ac905cfd6ec6646e5260a9be2c62b4dc61787f21
Nathan Stouffer <nathanstouffer1999@gmail.com>
2024-02-03 16:23:10 -0700

    tweak constants in aabb

--- code/src/stf/include/interface/stf/math/aabb.hpp
+++ code/src/stf/include/interface/stf/math/aabb.hpp
@@ -7,8 +7,8 @@
 #include "stf/math/constants.hpp"
 #include "stf/math/vector.hpp"
 
-namespace stf {
-namespace math {
+namespace stf::math
+{
 
     template<typename T, size_t N>
     struct aabb
@@ -27,26 +27,26 @@ namespace math {
         
         // call-through constructors
         aabb(vec_t const& _min, T const length) : aabb(_min, min + vec_t(length)) {}
-        aabb() : aabb(vec_t(std::numeric_limits<T>::lowest()), vec_t(std::numeric_limits<T>::max())) {}
+        aabb() : aabb(vec_t(constants<T>::neg_inf), vec_t(constants<T>::pos_inf)) {}
 
         // TODO (stouff) write a vertex indexing function
 
         inline vec_t const length() const { return max - min; }
-        inline vec_t const center() const { return min + (static_cast<T>(0.5) * length()); }
+        inline vec_t const center() const { return min + (constants<T>::half * length()); }
 
         T volume() const
         {
             T const len = length();
-            T measure = T(1);
+            T measure = constants<T>::one;
             for (size_t i = 0; i < N; ++i)
             {
-                if (len[i] < T(0))
+                if (len[i] < constants<T>::zero)
                 {
                     measure *= len[i];
                 }
                 else
                 {
-                    measure = T(0);
+                    measure = constants<T>::zero;
                 }
             }
         }
@@ -54,14 +54,15 @@ namespace math {
     public:
 
         static aabb everything() { return aabb(); }
-        static aabb nothing() { return aabb(vec_t(std::numeric_limits<T>::max()), vec_t(std::numeric_limits<T>::lowest())); }
-        static aabb unit() { return aabb(vec_t(0), vec_t(1)); }
+        static aabb nothing() { return aabb(vec_t(constants<T>::pos_inf), vec_t(constants<T>::neg_inf)); }
+        static aabb unit() { return aabb(vec_t(constants<T>::zero), vec_t(constants<T>::one)); }
 
     };
 
     // delete invalid aabb specialization
     template<typename T> struct aabb<T, 0> { aabb() = delete; };
 
+    // TODO (stouff) move this to a member function?
     template<typename T, size_t N>
     aabb<T, N> const fit(aabb<T, N> const& lhs, aabb<T, N> const& rhs)
     {
@@ -81,5 +82,4 @@ namespace math {
         return s
     }
 
-} // math
-} // stf
+} // stf::math
