b3d14faf5c3a87be2754023ae9ce39fd32b7a325
Nathan Stouffer <nathanstouffer1999@gmail.com>
2024-02-04 13:08:30 -0700

    add basic line segment class

---
+++ code/src/stf/include/interface/stf/math/segment.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "stf/math/constants.hpp"
+#include "stf/math/vector.hpp"
+
+namespace stf::math
+{
+
+    template<typename T, size_t N>
+    struct segment
+    {
+
+        using vec_t = vec<T, N>;
+
+    public:
+
+        vec_t a;
+        vec_t b;
+
+        segment() : segment(vec_t(constants<T>::zero), vec_t(constants<T>::zero)) {}
+        segment(vec_t const& _a, vec_t const& _b) : a(_a), b(_b) {}
+
+        inline T length() const { return (b - a).length(); }
+        inline vec_t delta() const { return b - a; }
+        inline vec_t direction() const { return (b - a).normalize(); }
+
+        inline T distance_to(vec_t const& x) const
+        {
+            vec_t diff = delta();
+            T scalar = ((x - a) * diff) / (diff * diff);
+            T t = std::max(constants<T>::zero, std::min(constants<T>::one, scalar));
+            vec_t proj = a + t * diff;
+            return (x - proj).length();
+        }
+
+        vec_t interpolate(T const t) const
+        {
+            if (t < constants<T>::zero)
+            {
+                return a;
+            }
+            else if (t < constants<T>::one)
+            {
+                return (constants<T>::one - t) * a + t * b;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+    };
+
+} // stf::math
