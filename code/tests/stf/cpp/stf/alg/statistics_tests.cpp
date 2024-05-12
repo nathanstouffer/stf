d5b49303836dbbecc518c65f31405cd6cd4dd4f2
Nathan Stouffer <nathanstouffer1999@gmail.com>
2024-05-12 08:46:45 -0600

    add median tests

--- code/tests/stf/cpp/stf/alg/statistics_tests.cpp
+++ code/tests/stf/cpp/stf/alg/statistics_tests.cpp
@@ -13,7 +13,12 @@ namespace stf::alg
     {
         std::vector<scaffolding::statistics::median<float>> tests =
         {
-            { { 0.f }, 0.f  },
+            { { 0.f }, 0.f },
+            { { 1.f }, 1.f },
+            { { 1.f, 3.f }, 2.f },
+            { { 3.f, 1.f }, 2.f },
+            { { 6.f, 3.f, 1.f }, 3.f },
+            { { -5.f, -4.f, -12.f, 4.f, 5.f }, -4.f },
         };
 
         for (scaffolding::statistics::median<float> const& test : tests)
