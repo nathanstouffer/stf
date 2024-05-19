66d2dcb235fa8e804fdb1b6ad89c6c2a9d2675c4
Nathan Stouffer <nathanstouffer1999@gmail.com>
2024-05-18 18:19:43 -0600

    add query iterators to interval_tree

--- code/src/stf/include/interface/stf/spatial/interval_tree.hpp
+++ code/src/stf/include/interface/stf/spatial/interval_tree.hpp
@@ -26,12 +26,6 @@ namespace stf::spatial
 
 		};
 
-	public:
-
-		explicit interval_tree(std::vector<entry_t>&& entries) : m_entries(std::move(entries)), m_root(interval_tree::construct(factory_args(m_entries))) {}
-
-		// TODO (stouff) write query function
-
 	private:
 
 		using entry_ptr_t = entry_t const*;
@@ -78,6 +72,178 @@ namespace stf::spatial
 			inline bool has_right() const { return right; }
 
 		};
+
+	private:
+
+		struct query_iterator
+		{
+
+			struct position_t
+			{
+				using iterator = typename std::vector<entry_ptr_t>::const_iterator;
+				
+				node_t const* node;
+				iterator it;
+
+				inline bool is_end() const { return !node; }
+
+				bool contains(T const query) const
+				{
+					if (node)
+					{
+						center_t const& center = node->center;
+						if (!center.is_empty())
+						{
+							return (*it)->interval.contains(query, boundary_types::CLOSED);
+						}
+					}
+					return false;	// fall-through to false
+				}
+
+				// move the position to the next available interval that might contain the query (jumps nodes if necessary)
+				void advance(T const query)
+				{
+					if (node)
+					{
+						center_t const& center = node->center;
+						if (query <= center.pivot)	// check if we need to jump nodes
+						{
+							// if any of the following are true, we jump nodes
+							//     1. this iterator is the end (checks the edge case where we constructed a position that points to an empty node)
+							//     2. the iterator points to a range that does not contain the query
+							//     3. the incremented iterator is the end
+							if (it == center.lesser.end() || query < (*it)->interval.a || ++it == center.lesser.end())
+							{
+								jump(query);
+							}
+						}
+						else
+						{
+							// if any of the following are true, we jump nodes
+							//     1. this iterator is the end (checks the edge case where we constructed a position that points to an empty node)
+							//     2. the iterator points to a range that does not contain the query
+							//     3. the incremented iterator is the greater
+							if (it == center.greater.end() || query < (*it)->interval.b || ++it == center.greater.end())
+							{
+								jump(query);
+							}
+						}
+					}
+				}
+
+			private:
+
+				// assign the iterator to the appropriate value (may call jump if there is no iterator to assign at the current node)
+				void assign(T const query)
+				{
+					if (node)
+					{
+						center_t const& center = node->center;
+						if (center.is_empty()) { jump(query); }		// if there are no intervals at this node, jump to the next
+						else
+						{
+							std::vector<entry_ptr_t> const& sorted = (query <= center.pivot) ? center.lesser : center.greater;
+							it = sorted.begin();
+						}
+					}
+				}
+
+				// jump to the next relevant node, then assign the iterator
+				void jump(T const query)
+				{
+					if (node)
+					{
+						center_t const& center = node->center;
+						if (query == center.pivot) { node = nullptr; }	// if the query is the pivot, no other nodes will contain it. set the iterator to end
+						else if (query < center.pivot)					// if the query is less than the pivot, take the left child
+						{
+							node = node->left.get();
+							assign(query);
+						}
+						else											// if the query is greater than the pivot, take the right child
+						{
+							node = node->right.get();
+							assign(query);
+						}
+					}
+				}
+
+			};
+
+			// iterator forward until one of the following conditions is true
+			//      1. we are at the end
+			//      2. the position points to an interval that contains the query point
+			void slide()
+			{
+				while (!m_position.is_end() && !m_position.contains(m_query))
+				{
+					m_position.advance(query);
+				}
+			}
+
+			query_iterator(position_t const& position, T const query) : m_position(position), m_query(query) {}
+
+			// unfortunately, we have to overload operator!= for range-based for loops to work. but this is NOT true equality
+			inline bool operator==(query_iterator const& rhs) const { return m_position.is_end() && rhs.m_position.is_end(); }
+			inline bool operator!=(query_iterator const& rhs) const { return !(*this == rhs); }
+
+			inline entry_t const& operator*() { return **m_position.it; }
+
+			inline query_iterator& operator++()
+			{
+				m_position.advance(m_query);	// advance once immediately
+				slide();						// slide forward until we hit something that matches the query
+				return *this;
+			}
+
+			inline query_iterator operator++(int)
+			{
+				query_iterator ret = *this;
+				++(*this);
+				return ret;
+			}
+
+		private:
+
+			position_t m_position;
+			T const m_query;
+
+		};
+
+	public:
+
+		struct query_range
+		{
+			
+			query_range(query_iterator begin, query_iterator end) : m_begin(begin), m_end(end) {}
+
+			inline query_iterator begin() const { return m_begin; }
+			inline query_iterator end() const { return m_end; }
+
+		private:
+
+			query_iterator m_begin;
+			query_iterator m_end;
+		
+		};
+
+	public:
+
+		explicit interval_tree(std::vector<entry_t>&& entries) : m_entries(std::move(entries)), m_root(interval_tree::construct(factory_args(m_entries))) {}
+
+		query_range find(T const query) const
+		{
+			static std::vector<entry_ptr_t> const s_flag;
+			query_iterator end(query_iterator::position_t(nullptr, s_flag.end()), query);
+			if (m_entries.empty()) { return query_range(end, end); }	// if there are no intervals, return an empty range
+			else
+			{
+				center_t const& center = m_root.center;
+				query_iterator::position_t::iterator it = (query <= center.pivot) ? center.lesser.cbegin() : center.greater.cbegin();
+				query_iterator begin(query_iterator::position_t(m_root.get(), it), query);
+				return query_range(begin, end);
+			}
+		}
 
 	private:
 
