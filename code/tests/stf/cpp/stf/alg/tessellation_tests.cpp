aba5767044845f47e681cdf1bfffbbbec1e2d6f4
Nathan Stouffer <nathanstouffer1999@gmail.com>
2024-08-04 14:24:59 -0600

    add tests for closing the polyline when tessellating

--- code/tests/stf/cpp/stf/alg/tessellation_tests.cpp
+++ code/tests/stf/cpp/stf/alg/tessellation_tests.cpp
@@ -20,6 +20,8 @@ namespace stf::alg
             { stff::polyline2({ stff::vec2(0), stff::vec2(0.8,0) }), 0.5f, false, { stff::vec2(0), stff::vec2(0.4,0), stff::vec2(0.8,0) } },
             { stff::polyline2({ stff::vec2(0), stff::vec2(0.9,0) }), 0.3f, false, { stff::vec2(0), stff::vec2(0.3,0), stff::vec2(0.6,0), stff::vec2(0.9,0) } },
             { stff::polyline2({ stff::vec2(0), stff::vec2(1,0), stff::vec2(2,1), stff::vec2(3,0), stff::vec2(4,0) }), 1.f, false, { stff::vec2(0), stff::vec2(1,0), stff::vec2(1.5,0.5), stff::vec2(2,1), stff::vec2(2.5,0.5), stff::vec2(3,0), stff::vec2(4,0) } },
+            { stff::polyline2({ stff::vec2(0), stff::vec2(1,0), stff::vec2(1) }), 1.f, false, { stff::vec2(0), stff::vec2(1,0), stff::vec2(1), } },
+            { stff::polyline2({ stff::vec2(0), stff::vec2(1,0), stff::vec2(1) }), 1.f, true, { stff::vec2(0), stff::vec2(1,0), stff::vec2(1), stff::vec2(0.5) }},
         };
         
         for (scaffolding::tessellation::polyline_via_length<float> const& test : tests)
