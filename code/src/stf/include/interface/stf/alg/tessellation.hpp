47e94de8dc28a12563ac424dafda7d5178f22352
Nathan Stouffer <nathanstouffer1999@gmail.com>
2024-08-04 14:18:14 -0600

    pull segment tessellation into a separate function for easy reuse

--- code/src/stf/include/interface/stf/alg/tessellation.hpp
+++ code/src/stf/include/interface/stf/alg/tessellation.hpp
@@ -14,11 +14,28 @@
 namespace stf::alg
 {
 
+    template<typename T>
+    void tessellate_via_length(std::vector<math::vec2<T>>& vertices, geom::segment2<T> const& segment, T const max_len)
+    {
+        vertices.push_back(segment.a);    // immediately push the first vertex in the segment
+        T const length = segment.length();
+        if (length > max_len)    // if necessary, add tessellated vertices
+        {
+            size_t count = static_cast<size_t>(std::ceil(length / max_len - math::constants<T>::one));  // the number of vertices we add to the interior of the segment
+            T const delta_t = math::constants<T>::one / static_cast<T>(count + 1);
+            for (size_t j = 1; j <= count; ++j)
+            {
+                vertices.push_back(segment.interpolate(static_cast<T>(j) * delta_t));
+            }
+        }
+    }
+
     /**
      * @brief Tessellate a polyline adding vertices so that no segment is longer than a specified length
      * @tparam T Number type (eg float)
      * @param [in] polyline The polyline to be tessellated
      * @param [in] max_len The maximum segment length allowed in the returned sequence of vertices
+     * @param [in] loop Whether or not the polyline should be closed with an additional segement from the back to the front
      * @return A std::vector of the tessellated vertices
      */
     template<typename T>
@@ -31,23 +48,13 @@ namespace stf::alg
         for (size_t i = 0; i + 1 < polyline.size(); ++i)    // iterate over all polyline edges, adding vertices
         {
             geom::segment2<T> edge = polyline.edge(i);
-            vertices.push_back(edge.a);    // immediately push the first vertex in the segment
-
-            T const length = edge.length();
-            if (length > max_len)    // if necessary, add tessellated vertices
-            {
-                size_t count = static_cast<size_t>(std::ceil(length / max_len - math::constants<T>::one));  // the number of vertices we add to the interior of the segment
-                T const delta_t = math::constants<T>::one / static_cast<T>(count + 1);
-                for (size_t j = 1; j <= count; ++j)
-                {
-                    vertices.push_back(edge.interpolate(static_cast<T>(j) * delta_t));
-                }
-            }
+            tessellate_via_length(vertices, edge, max_len);
         }
 
         if (loop)
         {
-
+            geom::segment2<T> edge(polyline.vertices().back(), polyline.vertices().front());
+            tessellate_via_length(vertices, edge, max_len);
         }
         else
         {
