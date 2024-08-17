9cf4fb14091f50f1da52fa4ae32078b669e3d944
Nathan Stouffer <nathanstouffer1999@gmail.com>
2024-08-16 19:05:29 -0600

    start to build out frustum intersect aabb tests

--- code/tests/stf/cpp/stf/cam/frustum_tests.cpp
+++ code/tests/stf/cpp/stf/cam/frustum_tests.cpp
@@ -11,9 +11,12 @@ namespace stf::cam
 
     TEST(frustum, intersects_fast)
     {
+        stff::scamera camera(stff::vec3(0), stff::constants::pi_halves, stff::constants::pi_halves, 0.1f, 100.f);
+        stff::frustum frustum(camera);
+
         std::vector<scaffolding::frustum::intersects_fast<float>> tests =
         {
-            // TODO (stouff) write this test
+            { frustum, stff::aabb3(stff::vec3(-1, 1, -1), stff::vec3(1, 1, 1)), true, false },
         };
 
         for (scaffolding::frustum::intersects_fast<float> const& test : tests) { scaffolding::frustum::verify(test); }
