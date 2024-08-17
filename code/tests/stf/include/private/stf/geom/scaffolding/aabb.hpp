d3dca408a2470e4337d33e4bda031311c3bd2a69
Nathan Stouffer <nathanstouffer1999@gmail.com>
2024-08-16 18:41:14 -0600

    add tests for dist_squared_to

--- code/tests/stf/include/private/stf/geom/scaffolding/aabb.hpp
+++ code/tests/stf/include/private/stf/geom/scaffolding/aabb.hpp
@@ -42,4 +42,19 @@ namespace stf::geom::scaffolding::aabb
         ASSERT_EQ(test.contained, alg::contains(test.lhs, test.rhs)) << "failed lhs -> rhs containment test (free)";
     }
 
+    template<typename T, size_t N>
+    struct dist_squared_to
+    {
+        geom::aabb<T, N> const box;
+        math::vec<T, N> const point;
+        T dist_squared;
+    };
+
+    template<typename T, size_t N>
+    void verify(dist_squared_to<T, N> const& test)
+    {
+        ASSERT_EQ(std::sqrt(test.dist_squared), test.box.dist_to(test.point)) << "failed distance from aabb to vec";
+        ASSERT_EQ(test.dist_squared, test.box.dist_squared_to(test.point)) << "failed distance squared from aabb to vec";
+    }
+
 } // stf::math::scaffolding::geom
