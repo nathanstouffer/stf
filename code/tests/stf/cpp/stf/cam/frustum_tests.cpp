d3d68c56bc051fc7d11f566a20cf253fde1d591e
Nathan Stouffer <nathanstouffer1999@gmail.com>
2024-09-07 17:21:19 -0600

    add false positive tests to intersection

--- code/tests/stf/cpp/stf/cam/frustum_tests.cpp
+++ code/tests/stf/cpp/stf/cam/frustum_tests.cpp
@@ -11,6 +11,7 @@ namespace stf::cam
 
     TEST(frustum, contains)
     {
+        // camera with the eye at the origin that looks along the +y axis
         stfd::scamera camera(stfd::vec3(0), stfd::constants::pi_halves, stfd::constants::pi_halves, 1.0, 100.0, 1.0, stfd::constants::pi_halves);
         stfd::frustum frustum(camera);
 
@@ -32,22 +33,46 @@ namespace stf::cam
 
     TEST(frustum, intersects_fast)
     {
-        stfd::scamera camera(stfd::vec3(0), stfd::constants::pi_halves, stfd::constants::pi_halves, 1.0, 100.0, 1.0, stfd::constants::pi_halves);
-        stfd::frustum frustum(camera);
-
-        std::vector<scaffolding::frustum::intersects_fast<double>> tests =
+        // true intersections
         {
-            { frustum, stfd::aabb3(stfd::vec3(-1, 1, -1), 2), true, false },
-            { frustum, stfd::aabb3(stfd::vec3(-10, 50, -10), 20), true, false },
-            { frustum, stfd::aabb3(stfd::vec3(-10, 90, -10), 20), true, false },      // crosses the far plane
-            { frustum, stfd::aabb3(stfd::vec3(-1, 0, -1), 2), true, false },          // crosses the near plane
-            { frustum, stfd::aabb3(stfd::vec3(-10, 5, -10), 20), true, false },       // crosses the left plane
-            { frustum, stfd::aabb3(stfd::vec3(0, 5, -10), 20), true, false },         // crosses the right plane
-            { frustum, stfd::aabb3(stfd::vec3(-10, 5, 0), 20), true, false },         // crosses the top plane
-            { frustum, stfd::aabb3(stfd::vec3(-10, 5, -10), 20), true, false },       // crosses the bottom plane
-        };
+            // camera with the eye at the origin that looks along the +y axis with the +z axis is the up vector
+            stfd::scamera camera(stfd::vec3(0), stfd::constants::pi_halves, stfd::constants::pi_halves, 1.0, 100.0, 1.0, stfd::constants::pi_halves);
+            stfd::frustum frustum(camera);
 
-        for (scaffolding::frustum::intersects_fast<double> const& test : tests) { scaffolding::frustum::verify(test); }
+            std::vector<scaffolding::frustum::intersects_fast<double>> tests =
+            {
+                { frustum, stfd::aabb3(stfd::vec3(-1, 1, -1), 2), true },
+                { frustum, stfd::aabb3(stfd::vec3(-10, 50, -10), 20), true },
+                { frustum, stfd::aabb3(stfd::vec3(-10, 90, -10), 20), true },       // crosses the far plane
+                { frustum, stfd::aabb3(stfd::vec3(-1, 0, -1), 2), true },           // crosses the near plane
+                { frustum, stfd::aabb3(stfd::vec3(-10, 5, -10), 20), true },        // crosses the left plane
+                { frustum, stfd::aabb3(stfd::vec3(0, 5, -10), 20), true },          // crosses the right plane
+                { frustum, stfd::aabb3(stfd::vec3(-10, 5, 0), 20), true },          // crosses the top plane
+                { frustum, stfd::aabb3(stfd::vec3(-10, 5, -10), 20), true },        // crosses the bottom plane
+            };
+
+            for (scaffolding::frustum::intersects_fast<double> const& test : tests) { scaffolding::frustum::verify(test); }
+        }
+
+        // false positives
+        {
+            // camera with the eye at the origin that looks along the -z axis with vec3(1, 1, 0) defining the up direction
+            stfd::scamera camera(stfd::vec3(0), stfd::constants::pi_fourths, stfd::constants::pi, 1.0, 100.0, 1.0, stfd::constants::pi_halves);
+            stfd::frustum frustum(camera);
+
+            // TODO (stouff) remove this once we improve the test
+            // camera with the eye at the origin that looks along the +y axis with the +z axis is the up vector
+            stfd::scamera old_camera(stfd::vec3(0), stfd::constants::pi_halves, stfd::constants::pi_halves, 1.0, 100.0, 1.0, stfd::constants::pi_halves);
+            stfd::frustum old_frustum(old_camera);
+
+            std::vector<scaffolding::frustum::intersects_fast<double>> tests =
+            {
+                { old_frustum, stfd::aabb3(stfd::vec3(-610, 90, -10), stfd::vec3(-110, 590, 10)), true },   // TODO (stouff) move this above once we improve the intersection alg
+                { frustum, stfd::aabb3(stfd::vec3(-500, 50, -10), stfd::vec3(500, 550, 10)), true },
+            };
+
+            for (scaffolding::frustum::intersects_fast<double> const& test : tests) { scaffolding::frustum::verify(test); }
+        }
     }
 
 } // stf::cam
--- code/tests/stf/include/private/stf/cam/scaffolding/frustum.hpp
+++ code/tests/stf/include/private/stf/cam/scaffolding/frustum.hpp
@@ -30,15 +30,14 @@ namespace stf::cam::scaffolding::frustum
         cam::frustum<T> const frustum;
         geom::aabb3<T> const box;
         bool const intersects;
-        bool const false_positive;
     };
 
     template<typename T>
     void verify(intersects_fast<T> const& test)
     {
-        ASSERT_EQ(test.intersects || test.false_positive, test.frustum.intersects_fast(test.box)) << "Failed to compute frustum::intersects_fast";
-        ASSERT_EQ(test.intersects || test.false_positive, alg::intersect_fast(test.frustum, test.box)) << "Failed to compute intersect_fast for frustum -> box";
-        ASSERT_EQ(test.intersects || test.false_positive, alg::intersect_fast(test.box, test.frustum)) << "Failed to compute intersect_fast for box -> frustum";
+        ASSERT_EQ(test.intersects, test.frustum.intersects_fast(test.box)) << "Failed to compute frustum::intersects_fast";
+        ASSERT_EQ(test.intersects, alg::intersect_fast(test.frustum, test.box)) << "Failed to compute intersect_fast for frustum -> box";
+        ASSERT_EQ(test.intersects, alg::intersect_fast(test.box, test.frustum)) << "Failed to compute intersect_fast for box -> frustum";
     }
 
 } // stf::cam::scaffolding::frustum
