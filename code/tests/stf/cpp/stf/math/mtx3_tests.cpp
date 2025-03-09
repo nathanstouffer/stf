0004e9b3de8afde69eb2b5e199dc0333f82701bf
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-03-09 17:21:14 -0600

    add mtx3 determinant tests

--- code/tests/stf/cpp/stf/math/mtx3_tests.cpp
+++ code/tests/stf/cpp/stf/math/mtx3_tests.cpp
@@ -21,6 +21,21 @@ namespace stf::math
         for (scaffolding::mtx::equality<float, 3> const& test : tests) { scaffolding::mtx::verify(test); }
     }
 
+    TEST(mtx3, determinant)
+    {
+        std::vector<scaffolding::mtx::determinant<float, 3>> tests =
+        {
+            //{ stff::mtx3(), 1 },
+            { stff::mtx3(stff::vec3(2)), 8 },
+            //{ stff::mtx3(stff::vec3(-2)), 8 },
+            //{ stff::mtx3(stff::vec3(-2, 2, 2)), -8 },
+            //{ stff::mtx3(stff::vec<9>(2)), 0 },
+            //{ stff::mtx3(stff::vec<9>(2, 1, -3, 5)), 13 },
+        };
+
+        for (scaffolding::mtx::determinant<float, 3> const& test : tests) { scaffolding::mtx::verify(test); }
+    }
+
     TEST(mtx3, inverse)
     {
         std::vector<scaffolding::mtx::inverse<float, 3>> tests =
