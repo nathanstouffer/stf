472dc95b511cd036b364534d583f1c99c825c280
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-03-18 21:38:26 -0600

    tweak test scaffolding

--- code/tests/stf/include/private/stf/alg/scaffolding/intersect.hpp
+++ code/tests/stf/include/private/stf/alg/scaffolding/intersect.hpp
@@ -22,17 +22,17 @@ namespace stf::alg::scaffolding::intersect
         geom::segment2<T> reverse_lhs(test.lhs.b, test.lhs.a);
         geom::segment2<T> reverse_rhs(test.rhs.b, test.rhs.a);
 
-        ASSERT_EQ(test.expected, alg::intersect(test.lhs, test.rhs)) << "Failed to intersect(lhs, rhs)";
-        ASSERT_EQ(test.expected, alg::intersect(test.rhs, test.lhs)) << "Failed to intersect(rhs, lhs)";
+        ASSERT_EQ(test.expected, alg::intersect(test.lhs, test.rhs)) << "Failed intersect(lhs, rhs)";
+        ASSERT_EQ(test.expected, alg::intersect(test.rhs, test.lhs)) << "Failed intersect(rhs, lhs)";
 
-        ASSERT_EQ(test.expected, alg::intersect(reverse_lhs, test.rhs)) << "Failed to intersect(reverse_lhs, rhs)";
-        ASSERT_EQ(test.expected, alg::intersect(test.rhs, reverse_lhs)) << "Failed to intersect(rhs, reverse_lhs)";
+        ASSERT_EQ(test.expected, alg::intersect(reverse_lhs, test.rhs)) << "Failed intersect(reverse_lhs, rhs)";
+        ASSERT_EQ(test.expected, alg::intersect(test.rhs, reverse_lhs)) << "Failed intersect(rhs, reverse_lhs)";
 
-        ASSERT_EQ(test.expected, alg::intersect(test.lhs, reverse_rhs)) << "Failed to intersect(lhs, reverse_rhs)";
-        ASSERT_EQ(test.expected, alg::intersect(reverse_rhs, test.lhs)) << "Failed to intersect(reverse_rhs, lhs)";
+        ASSERT_EQ(test.expected, alg::intersect(test.lhs, reverse_rhs)) << "Failed intersect(lhs, reverse_rhs)";
+        ASSERT_EQ(test.expected, alg::intersect(reverse_rhs, test.lhs)) << "Failed intersect(reverse_rhs, lhs)";
 
-        ASSERT_EQ(test.expected, alg::intersect(reverse_lhs, reverse_rhs)) << "Failed to intersect(reverse_lhs, reverse_rhs)";
-        ASSERT_EQ(test.expected, alg::intersect(reverse_rhs, reverse_lhs)) << "Failed to intersect(reverse_rhs, reverse_lhs)";
+        ASSERT_EQ(test.expected, alg::intersect(reverse_lhs, reverse_rhs)) << "Failed intersect(reverse_lhs, reverse_rhs)";
+        ASSERT_EQ(test.expected, alg::intersect(reverse_rhs, reverse_lhs)) << "Failed intersect(reverse_rhs, reverse_lhs)";
     }
 
     template<typename T>
@@ -48,11 +48,11 @@ namespace stf::alg::scaffolding::intersect
     {
         geom::segment2<T> reverse_seg(test.seg.b, test.seg.a);
 
-        ASSERT_EQ(test.expected, alg::intersect(test.seg, test.box)) << "Failed to intersect(seg, box)";
-        ASSERT_EQ(test.expected, alg::intersect(test.box, test.seg)) << "Failed to intersect(box, seg)";
+        ASSERT_EQ(test.expected, alg::intersect(test.seg, test.box)) << "Failed intersect(seg, box)";
+        ASSERT_EQ(test.expected, alg::intersect(test.box, test.seg)) << "Failed intersect(box, seg)";
 
-        ASSERT_EQ(test.expected, alg::intersect(reverse_seg, test.box)) << "Failed to intersect(reverse_seg, box)";
-        ASSERT_EQ(test.expected, alg::intersect(test.box, reverse_seg)) << "Failed to intersect(box, reverse_seg)";
+        ASSERT_EQ(test.expected, alg::intersect(reverse_seg, test.box)) << "Failed intersect(reverse_seg, box)";
+        ASSERT_EQ(test.expected, alg::intersect(test.box, reverse_seg)) << "Failed intersect(box, reverse_seg)";
     }
 
     template<typename T>
@@ -66,8 +66,8 @@ namespace stf::alg::scaffolding::intersect
     template<typename T>
     void verify(polyline_with_aabb<T> const& test)
     {
-        ASSERT_EQ(test.expected, alg::intersect(test.polyline, test.box)) << "Failed to intersect(polyline, box)";
-        ASSERT_EQ(test.expected, alg::intersect(test.box, test.polyline)) << "Failed to intersect(box, polyline)";
+        ASSERT_EQ(test.expected, alg::intersect(test.polyline, test.box)) << "Failed intersect(polyline, box)";
+        ASSERT_EQ(test.expected, alg::intersect(test.box, test.polyline)) << "Failed intersect(box, polyline)";
     }
 
     template<typename T>
@@ -81,8 +81,8 @@ namespace stf::alg::scaffolding::intersect
     template<typename T>
     void verify(polygon_with_aabb<T> const& test)
     {
-        ASSERT_EQ(test.expected, alg::intersect(test.polygon, test.box)) << "Failed to intersect(polygon, box)";
-        ASSERT_EQ(test.expected, alg::intersect(test.box, test.polygon)) << "Failed to intersect(box, polygon)";
+        ASSERT_EQ(test.expected, alg::intersect(test.polygon, test.box)) << "Failed intersect(polygon, box)";
+        ASSERT_EQ(test.expected, alg::intersect(test.box, test.polygon)) << "Failed intersect(box, polygon)";
     }
 
     template<typename T>
@@ -96,8 +96,8 @@ namespace stf::alg::scaffolding::intersect
     template<typename T>
     void verify(polygon_with_segment<T> const& test)
     {
-        ASSERT_EQ(test.expected, alg::intersect(test.polygon, test.segment)) << "Failed to intersect(polygon, segment)";
-        ASSERT_EQ(test.expected, alg::intersect(test.segment, test.polygon)) << "Failed to intersect(segment, polygon)";
+        ASSERT_EQ(test.expected, alg::intersect(test.polygon, test.segment)) << "Failed intersect(polygon, segment)";
+        ASSERT_EQ(test.expected, alg::intersect(test.segment, test.polygon)) << "Failed intersect(segment, polygon)";
     }
 
     template<typename T>
@@ -111,8 +111,8 @@ namespace stf::alg::scaffolding::intersect
     template<typename T>
     void verify(polygon_with_polyline<T> const& test)
     {
-        ASSERT_EQ(test.expected, alg::intersect(test.polygon, test.polyline)) << "Failed to intersect(polygon, polyline)";
-        ASSERT_EQ(test.expected, alg::intersect(test.polyline, test.polygon)) << "Failed to intersect(polyline, polygon)";
+        ASSERT_EQ(test.expected, alg::intersect(test.polygon, test.polyline)) << "Failed intersect(polygon, polyline)";
+        ASSERT_EQ(test.expected, alg::intersect(test.polyline, test.polygon)) << "Failed intersect(polyline, polygon)";
     }
 
 } // stf::alg::scaffolding::intersection
