7086a8b40a0de81370a0a9e4004fc6b5619990e8
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-04-10 10:15:18 -0600

    add address sanitizer to clang/gcc debug builds

---
+++ code/cmake/AddressSanitizer.cmake
@@ -0,0 +1,8 @@
+# set up the address sanitizer -- this is clang/gcc only
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -faddress=sanitize")
+    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -faddress=sanitize")
+    set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -faddress=sanitize")
+    set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -faddress=sanitize")
+    set(CMAKE_SHARED_LINKER_FLAGS_DEBUG "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -faddress=sanitize")
+endif()
