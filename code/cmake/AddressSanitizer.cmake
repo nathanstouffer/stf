20ad655857feb42218e3b539d724b3696debcf2b
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-04-10 11:18:59 -0600

    add debug statements

--- code/cmake/AddressSanitizer.cmake
+++ code/cmake/AddressSanitizer.cmake
@@ -2,6 +2,8 @@ if (ENABLE_ASAN)
     # set up the address sanitizer -- this is clang/gcc only
     set(ASAN_FLAGS "-fsanitize=address")
 
+    message("HERE")
+
     set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} ${ASAN_FLAGS}")
     set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} ${ASAN_FLAGS}")
     set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${ASAN_FLAGS}")
