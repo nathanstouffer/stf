5bbcfb4ee452a2157c661a9af2ee6fa32b3cdf99
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-04-15 21:17:08 -0600

    convert mtx3 and mtx4 tests to templated verification

--- code/tests/stf/cpp/stf/math/mtx3_tests.cpp
+++ code/tests/stf/cpp/stf/math/mtx3_tests.cpp
@@ -5,25 +5,25 @@
 #include <stf/stf.hpp>
 
 #include "stf/scaffolding/math/matrix.hpp"
+#include "stf/scaffolding/verify.hpp"
 
 namespace stf::math
 {
 
     TEST(mtx3, equality)
     {
-        std::vector<scaffolding::mtx::equality<float, 3>> tests =
+        std::vector<scaffolding::math::mtx::equality<float, 3>> tests =
         {
             { stff::mtx3(), stff::mtx3(), true },
             { stff::mtx3(1), stff::mtx3(1), true },
             { stff::mtx3(1), stff::mtx3(2), false },
         };
-
-        for (scaffolding::mtx::equality<float, 3> const& test : tests) { scaffolding::mtx::verify(test); }
+        scaffolding::verify(tests);
     }
 
     TEST(mtx3, determinant)
     {
-        std::vector<scaffolding::mtx::determinant<float, 3>> tests =
+        std::vector<scaffolding::math::mtx::determinant<float, 3>> tests =
         {
             { stff::mtx3(), 1 },
             { stff::mtx3(stff::vec3(2)), 8 },
@@ -32,13 +32,12 @@ namespace stf::math
             { stff::mtx3(stff::vec<9>(2)), 0 },
             { stff::mtx3(stff::vec<9>({ 2, 1, 0, -3, 5, 1, 1, -1, -2 })), -23 },
         };
-
-        for (scaffolding::mtx::determinant<float, 3> const& test : tests) { scaffolding::mtx::verify(test); }
+        scaffolding::verify(tests);
     }
 
     TEST(mtx3, inverted)
     {
-        std::vector<scaffolding::mtx::inverted<float, 3>> tests =
+        std::vector<scaffolding::math::mtx::inverted<float, 3>> tests =
         {
             { stff::mtx3() },
             { stff::mtx3(stff::vec3(2)) },
@@ -46,8 +45,7 @@ namespace stf::math
             { stf::math::rotate(stff::vec3(1), stff::constants::quarter_pi).prefix() },
             { (stf::math::rotate(stff::vec3(1), stff::constants::quarter_pi) * stf::math::scale(stff::vec3(1, 3, 2))).prefix() },
         };
-
-        for (scaffolding::mtx::inverted<float, 3> const& test : tests) { scaffolding::mtx::verify(test); }
+        scaffolding::verify(tests);
     }
 
 } // stf::math
--- code/tests/stf/cpp/stf/math/mtx4_tests.cpp
+++ code/tests/stf/cpp/stf/math/mtx4_tests.cpp
@@ -5,26 +5,26 @@
 #include <stf/stf.hpp>
 
 #include "stf/scaffolding/math/matrix.hpp"
+#include "stf/scaffolding/verify.hpp"
 
 namespace stf::math
 {
 
     TEST(mtx4, equality)
     {
-        std::vector<scaffolding::mtx::equality<float, 4>> tests =
+        std::vector<scaffolding::math::mtx::equality<float, 4>> tests =
         {
             { stff::mtx4(), stff::mtx4(), true },
             { stff::mtx4(1), stff::mtx4(1), true },
             { stff::mtx4(1), stff::mtx4(2), false },
             { stff::mtx4(), stff::mtx4(stff::vec<16>({ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1.1f })), false },
         };
-
-        for (scaffolding::mtx::equality<float, 4> const& test : tests) { scaffolding::mtx::verify(test); }
+        scaffolding::verify(tests);
     }
 
     TEST(mtx4, determinant)
     {
-        std::vector<scaffolding::mtx::determinant<float, 4>> tests =
+        std::vector<scaffolding::math::mtx::determinant<float, 4>> tests =
         {
             { stff::mtx4(), 1 },
             { stff::mtx4(stff::vec4(2)), 16 },
@@ -33,13 +33,12 @@ namespace stf::math
             { stff::mtx4(stff::vec<16>(2)), 0 },
             { stff::mtx4(stff::vec<16>({ 2, 1, 0, 0, -3, 5, 1, -1, 1, -1, -2, 2, -3, 2, -1, -1 })), 46 },
         };
-
-        for (scaffolding::mtx::determinant<float, 4> const& test : tests) { scaffolding::mtx::verify(test); }
+        scaffolding::verify(tests);
     }
 
     TEST(mtx4, inverted)
     {
-        std::vector<scaffolding::mtx::inverted<float, 4>> tests =
+        std::vector<scaffolding::math::mtx::inverted<float, 4>> tests =
         {
             { stff::mtx4() },
             { stff::mtx4(stff::vec4(2)) },
@@ -47,8 +46,7 @@ namespace stf::math
             { stf::math::rotate(stff::vec3(1), stff::constants::quarter_pi) },
             { stf::math::rotate(stff::vec3(1), stff::constants::quarter_pi) * stf::math::scale(stff::vec3(1, 3, 2)) },
         };
-
-        for (scaffolding::mtx::inverted<float, 4> const& test : tests) { scaffolding::mtx::verify(test); }
+        scaffolding::verify(tests);
     }
 
 } // stf::math
