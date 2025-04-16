0eda29b8f0c16284ca18b40a23418494c8f43993
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-04-15 21:34:41 -0600

    convert transform tests to templated verification

--- code/tests/stf/cpp/stf/math/transform_tests.cpp
+++ code/tests/stf/cpp/stf/math/transform_tests.cpp
@@ -6,26 +6,26 @@
 #include <stf/math/transform.hpp>
 
 #include "stf/scaffolding/math/transform.hpp"
+#include "stf/scaffolding/verify.hpp"
 
 namespace stf::math
 {
     
     TEST(transform, rotate2)
     {
-        std::vector<scaffolding::transform::rotate2<float>> tests = 
+        std::vector<scaffolding::math::transform::rotate2<float>> tests = 
         {
             // initial              rotation                    expected
             { stff::vec2(1, 0),     stff::constants::pi,        stff::vec2(-1, 0) },
             { stff::vec2(1, 0),     stff::constants::half_pi,   stff::vec2(0, 1) },
             { stff::vec2(1, 0),     -stff::constants::half_pi,  stff::vec2(0, -1) },
         };
-
-        for (scaffolding::transform::rotate2<float> const& test : tests) { scaffolding::transform::verify(test); }
+        scaffolding::verify(tests);
     }
 
     TEST(transform, orbit2)
     {
-        std::vector<scaffolding::transform::orbit2<float>> tests = 
+        std::vector<scaffolding::math::transform::orbit2<float>> tests = 
         {
             // initial              focus               rotation                    expected
             { stff::vec2(1, 0),     stff::vec2(0),      stff::constants::zero,      stff::vec2(1, 0) },
@@ -36,13 +36,12 @@ namespace stf::math
             { stff::vec2(1, 0),     stff::vec2(0),      -stff::constants::half_pi,  stff::vec2(0, -1) },
             { stff::vec2(4, 0),     stff::vec2(1, 0),   stff::constants::half_pi,   stff::vec2(1, 3) },
         };
-
-        for (scaffolding::transform::orbit2<float> const& test : tests) { scaffolding::transform::verify(test); }
+        scaffolding::verify(tests);
     }
 
     TEST(transform, rotate3)
     {
-        std::vector<scaffolding::transform::rotate3<float>> tests = 
+        std::vector<scaffolding::math::transform::rotate3<float>> tests = 
         {
             // initial              axis                                rotation                    expected
             // idempotent rotations
@@ -55,20 +54,18 @@ namespace stf::math
             { stff::vec3(1, 0, 0),  stff::vec3(0, 0, 1),                stff::constants::half_pi,   stff::vec3(0, 1, 0) },
             { stff::vec3(1, 0, 0),  stff::vec3(1, 0, 1).normalized(),   stff::constants::pi,        stff::vec3(0, 0, 1) },
         };
-
-        for (scaffolding::transform::rotate3<float> const& test : tests) { scaffolding::transform::verify(test); }
+        scaffolding::verify(tests);
     }
 
     TEST(transform, orbit3)
     {
-        std::vector<scaffolding::transform::orbit3<float>> tests = 
+        std::vector<scaffolding::math::transform::orbit3<float>> tests = 
         {
             // initial              focus           right                   delta phi                       delta theta                 expected
             { stff::vec3(0, -1, 0), stff::vec3(),   stff::vec3(1, 0, 0),    stff::constants::zero,          stff::constants::zero,      stff::vec3(0, -1, 0) },
             { stff::vec3(0, -1, 0), stff::vec3(),   stff::vec3(1, 0, 0),    -stff::constants::quarter_pi,   stff::constants::half_pi,   stff::vec3(0.707107f, 0, 0.707107f) },
         };
-
-        for (scaffolding::transform::orbit3<float> const& test : tests) { scaffolding::transform::verify(test); }
+        scaffolding::verify(tests);
     }
 
 } // stf::math
--- code/tests/stf/include/private/stf/scaffolding/math/transform.hpp
+++ code/tests/stf/include/private/stf/scaffolding/math/transform.hpp
@@ -11,67 +11,63 @@ namespace stf::scaffolding::math::transform
     template<typename T>
     struct rotate2
     {
-        math::vec<T, 2> initial;
+        stf::math::vec<T, 2> initial;
         T delta_theta;
-        math::vec<T, 2> expected;
-    };
+        stf::math::vec<T, 2> expected;
 
-    template<typename T>
-    void verify(rotate2<T> const& test)
-    {
-        ASSERT_EQ(test.expected, math::rotate(test.initial, test.delta_theta)) << "Failed rotate function";
-        ASSERT_EQ(test.expected, math::rotate(test.delta_theta) * test.initial) << "Failed rotate matrix";
-    }
+        void verify(size_t const i) const
+        {
+            ASSERT_EQ(expected, stf::math::rotate(initial, delta_theta)) << info(i) << "Failed rotate function";
+            ASSERT_EQ(expected, stf::math::rotate(delta_theta) * initial) << info(i) << "Failed rotate matrix";
+        }
+    };
 
     template<typename T>
     struct orbit2
     {
-        math::vec<T, 2> initial;
-        math::vec<T, 2> focus;
+        stf::math::vec<T, 2> initial;
+        stf::math::vec<T, 2> focus;
         T delta_theta;
-        math::vec<T, 2> expected;
-    };
+        stf::math::vec<T, 2> expected;
 
-    template<typename T>
-    void verify(orbit2<T> const& test)
-    {
-        ASSERT_EQ(test.expected, math::orbit(test.initial, test.focus, test.delta_theta)) << "Failed orbit function";
-    }
+        void verify(size_t const i) const
+        {
+            ASSERT_EQ(expected, stf::math::orbit(initial, focus, delta_theta)) << info(i) << "Failed orbit function";
+        }
+    };
 
     template<typename T>
     struct rotate3
     {
-        math::vec<T, 3> initial;
-        math::vec<T, 3> axis;
+        stf::math::vec<T, 3> initial;
+        stf::math::vec<T, 3> axis;
         T delta_theta;
-        math::vec<T, 3> expected;
-    };
+        stf::math::vec<T, 3> expected;
 
-    template<typename T>
-    void verify(rotate3<T> const& test)
-    {
-        using homogenized_t = math::vec<T, 4>;
-        ASSERT_EQ(test.expected, math::rotate(test.initial, test.axis, test.delta_theta)) << "Failed rotate function";
-        ASSERT_EQ(test.expected, (math::rotate(test.axis, test.delta_theta) * homogenized_t(test.initial, 1)).xyz) << "Failed rotate matrix";
-    }
+        void verify(size_t const i) const
+        {
+            using homogenized_t = stf::math::vec<T, 4>;
+            ASSERT_EQ(expected, stf::math::rotate(initial, axis, delta_theta)) << info(i) << "Failed rotate function";
+            ASSERT_EQ(expected, (stf::math::rotate(axis, delta_theta) * homogenized_t(initial, 1)).xyz) << info(i) << "Failed rotate matrix";
+        }
+    };
 
     template<typename T>
     struct orbit3
     {
-        math::vec<T, 3> initial;
-        math::vec<T, 3> focus;
-        math::vec<T, 3> right;
+        stf::math::vec<T, 3> initial;
+        stf::math::vec<T, 3> focus;
+        stf::math::vec<T, 3> right;
         T delta_phi;
         T delta_theta;
-        math::vec<T, 3> expected;
-    };
+        stf::math::vec<T, 3> expected;
 
-    template<typename T>
-    void verify(orbit3<T> const& test)
-    {
-        using homogenized_t = math::vec<T, 4>;
-        ASSERT_EQ(test.expected, math::orbit(test.initial, test.focus, test.right, test.delta_phi, test.delta_theta)) << "Failed orbit function";
-        ASSERT_EQ(test.expected, (math::orbit(test.focus, test.right, test.delta_phi, test.delta_theta) * homogenized_t(test.initial, 1)).xyz) << "Failed orbit matrix";
-    }
+        void verify(size_t const i) const
+        {
+            using homogenized_t = stf::math::vec<T, 4>;
+            ASSERT_EQ(expected, stf::math::orbit(initial, focus, right, delta_phi, delta_theta)) << info(i) << "Failed orbit function";
+            ASSERT_EQ(expected, (stf::math::orbit(focus, right, delta_phi, delta_theta) * homogenized_t(initial, 1)).xyz) << info(i) << "Failed orbit matrix";
+        }
+    };
 
 } // stf::scaffolding::math::transform
