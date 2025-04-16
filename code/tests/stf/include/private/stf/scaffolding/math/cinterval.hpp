95c50c97ada0a8a3b451c880854cf312fcf0ff6d
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-04-15 21:19:20 -0600

    convert cinterval tests to templated verification

--- code/tests/stf/include/private/stf/scaffolding/math/cinterval.hpp
+++ code/tests/stf/include/private/stf/scaffolding/math/cinterval.hpp
@@ -10,16 +10,15 @@ namespace stf::scaffolding::math::cinterval
     template<typename T>
     struct contains_point
     {
-        math::cinterval<T> interval;
+        stf::math::cinterval<T> interval;
         boundary_types type;
         T x;
         bool contained;
-    };
 
-    template<typename T>
-    void verify(contains_point<T> const& test)
-    {
-        ASSERT_EQ(test.contained, test.interval.contains(test.x, test.type)) << "failed contains point test";
-    }
+        void verify(size_t const i) const
+        {
+            ASSERT_EQ(contained, interval.contains(x, type)) << info(i) << "failed contains point test";
+        }
+    };
 
 } // stf::scaffolding::math::cinterval
