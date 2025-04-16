f91626ea06edcac2d2d610c2abc24d142c125b6c
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-04-15 21:41:16 -0600

    convert interval_tree tests to templated verification

--- code/tests/stf/include/private/stf/scaffolding/spatial/interval_tree.hpp
+++ code/tests/stf/include/private/stf/scaffolding/spatial/interval_tree.hpp
@@ -15,37 +15,34 @@ namespace stf::scaffolding::spatial::interval_tree
     struct find
     {
 
-        spatial::interval_tree<T, std::string> tree;
+        stf::spatial::interval_tree<T, std::string> tree;
         T query;
-        std::vector<typename spatial::interval_tree<T, std::string>::entry_t> expected; // not necessarily in order
+        std::vector<typename stf::spatial::interval_tree<T, std::string>::entry_t> expected; // not necessarily in order
 
+        void verify(size_t const index) const
+        {
+            std::vector<bool> indicators(expected.size(), false);
+            size_t count = 0;
+            for (typename stf::spatial::interval_tree<T, std::string>::entry_t const& entry : tree.find(query))
+            {
+                ++count;
+            
+                auto found = std::find(expected.begin(), expected.end(), entry);
+                ASSERT_NE(found, expected.end()) << info(index) << "interval_tree::find returned unexpected value: " + found->value;
+            
+                // update the indicators
+                size_t i = found - expected.begin();
+                indicators[i] = true;
+            }
+
+            // check that all entries were found
+            for (size_t i = 0; i < indicators.size(); ++i)
+            {
+                ASSERT_TRUE(indicators[i]) << info(index) << "Failed to find value: " + expected[i].value;
+            }
+            // check that the correct number of entries was found
+            ASSERT_EQ(expected.size(), count) << info(index) << "interval_tree::find returned an incorrect number of values";
+        }
     };
-
-    template<typename T>
-    void verify(find<T> const& test)
-    {
-
-        std::vector<bool> indicators(test.expected.size(), false);
-        size_t count = 0;
-        for (typename spatial::interval_tree<T, std::string>::entry_t const& entry : test.tree.find(test.query))
-        {
-            ++count;
-            
-            auto found = std::find(test.expected.begin(), test.expected.end(), entry);
-            ASSERT_NE(found, test.expected.end()) << "interval_tree::find returned unexpected value: " + found->value;
-            
-            // update the indicators
-            size_t i = found - test.expected.begin();
-            indicators[i] = true;
-        }
-
-        // check that all entries were found
-        for (size_t i = 0; i < indicators.size(); ++i)
-        {
-            ASSERT_TRUE(indicators[i]) << "Failed to find value: " + test.expected[i].value;
-        }
-        // check that the correct number of entries was found
-        ASSERT_EQ(test.expected.size(), count) << "interval_tree::find returned an incorrect number of values";
-    }
 
 } // stf::scaffolding::spatial::interval_tree
