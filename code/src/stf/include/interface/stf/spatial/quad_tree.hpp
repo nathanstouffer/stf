5e172c6188ead1069f863b498eb7a28ba3231dad
Nathan Stouffer <nathanstouffer1999@gmail.com>
2026-03-21 22:25:01 -0600

    add quadtree file

---
+++ code/src/stf/include/interface/stf/spatial/quad_tree.hpp
@@ -0,0 +1,26 @@
+#ifndef STF_SPATIAL_QUAD_TREE_HPP_HEADER_GUARD
+#define STF_SPATIAL_QUAD_TREE_HPP_HEADER_GUARD
+
+#include <algorithm>
+#include <memory>
+#include <vector>
+
+#include "stf/math/constants.hpp"
+#include "stf/math/interval.hpp"
+
+/**
+ * @file quad.hpp
+ * @brief A file containing a class that implements an quad tree
+ */
+
+namespace stf::spatial
+{
+
+class quad_tree
+{
+public:
+};
+
+} // namespace stf::spatial
+
+#endif
