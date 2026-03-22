d5c962089969ea8ef2ec545ab020386225059a37
Nathan Stouffer <nathanstouffer1999@gmail.com>
2026-03-21 22:25:47 -0600

    remove superfluous includes

--- code/src/stf/include/interface/stf/spatial/quad_tree.hpp
+++ code/src/stf/include/interface/stf/spatial/quad_tree.hpp
@@ -1,13 +1,6 @@
 #ifndef STF_SPATIAL_QUAD_TREE_HPP_HEADER_GUARD
 #define STF_SPATIAL_QUAD_TREE_HPP_HEADER_GUARD
 
-#include <algorithm>
-#include <memory>
-#include <vector>
-
-#include "stf/math/constants.hpp"
-#include "stf/math/interval.hpp"
-
 /**
  * @file quad.hpp
  * @brief A file containing a class that implements an quad tree
